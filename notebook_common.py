04f54bf6adbd1e1cb0dbb1534d206bfb1e239045
OliverWong <hopensic@gmail.com>
2025-06-07 21:47:28 +0800

    update

--- notebook_common.py
+++ notebook_common.py
@@ -119,6 +119,16 @@ def check_encoding(file_path):
         print(f"检测到编码: {result['encoding']}")
 
 
+def rd_csv_sig(path, index_columns=None):
+    return pd.read_csv(
+        path,
+        encoding="utf-8-sig",
+        on_bad_lines="skip",
+        engine="python",
+        index_col=index_columns,
+    )
+
+
 def to_csv_sig(df,path,need_index=False):        
     df.to_csv(path, index=need_index, encoding="utf-8-sig")
 
