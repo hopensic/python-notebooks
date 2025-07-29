0f0f2ec72ec7853d9387acc01dda2ca2f2246dc4
OliverWong <hopensic@gmail.com>
2025-07-29 21:15:00 +0800

    update

--- notebook_common.py
+++ notebook_common.py
@@ -140,6 +140,7 @@ simple_meaning_increase_30000_csv=meaning_url+"simple_meaning_increase_30000.csv
 w_merged_130000_simple_meaning_csv=meaning_url+"w_merged_130000_simple_meaning.csv"
 #第二次增加5000以内的词义
 w_incr_meaning_merged_v2_3084=meaning_url+"w_incr_meaning_merged_v2_3084.csv"
+w_incr_meaning_merged_v3_3023=meaning_url+"w_incr_meaning_merged_v3_3023.csv"
 
 
 
